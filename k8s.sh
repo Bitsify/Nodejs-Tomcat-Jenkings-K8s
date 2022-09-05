06737a44713dcc6f09071912a2d49b1877669d5d
Nikki Nice <93078324+Bitsify@users.noreply.github.com>
2022-09-05 17:39:34 +0530

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-scp deploymentservice.yml nnice@192.168.248.188:/home/nnice/Desktop && ssh nnice@192.168.248.188 '/home/nnice/Desktop/k8s-deploy.sh'
+scp deploymentservice.yml nnice@192.168.248.188:/home/nnice/Desktop
+ssh nnice@192.168.248.188 '/home/nnice/Desktop/k8s-deploy.sh'
